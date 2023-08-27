d4eb7d2282d9036db52e81fe43dbd4fbfaba3c65
Pedroh-Martinsc <pedrohenriquem0136@gmail.com>
2023-08-27 18:36:14 -0300

    Comentado outras maneiras de encontrar as tasks pelo user id

--- src/main/java/com/api/restful/simples/repositories/TaskRepository.java
+++ src/main/java/com/api/restful/simples/repositories/TaskRepository.java
@@ -2,8 +2,8 @@ package com.api.restful.simples.repositories;
 
 import com.api.restful.simples.models.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+// import org.springframework.data.jpa.repository.Query;
+// import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
