502a1fe2a70bd568aa3ddac7491ca3ca9ca93d87
Pedroh-Martinsc <pedrohenriquem0136@gmail.com>
2023-08-27 16:01:18 -0300

    Foi adicionado a anotação para atribuir multiplas tasks a um mesmo usuario e referenciar a task utilizando a variavel user da classe Task

--- src/main/java/com/api/restful/simples/repositories/UserRepository.java
+++ src/main/java/com/api/restful/simples/repositories/UserRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-// Todos métodos já foram incluídos após o extends no JpaRepository.
+// Todos métodos já foram incluídos após o extends no JpaRepository..
 public interface UserRepository extends JpaRepository<User, Long> {
 }
