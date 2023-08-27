f4575be6939e6f44a42ed2ac309d73d5410959d4
Pedroh-Martinsc <pedrohenriquem0136@gmail.com>
2023-08-27 18:16:16 -0300

    Classe principal com método construtor do spring boot

--- src/main/java/com/api/restful/simples/SimplesApplication.java
+++ src/main/java/com/api/restful/simples/SimplesApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SimplesApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SimplesApplication.class, args);
 	}
