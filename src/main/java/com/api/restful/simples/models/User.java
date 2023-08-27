b0563e4aeb53c1466de694470603bd923dc00948
Pedroh-Martinsc <pedrohenriquem0136@gmail.com>
2023-08-26 23:01:09 -0300

    Criação do modelo de usuário com validações, atributos, métodos

--- src/main/java/com/api/restful/simples/models/User.java
+++ src/main/java/com/api/restful/simples/models/User.java
@@ -14,34 +14,34 @@ import java.util.Objects;
 @Table(name = User.TABLE_NAME) // Definindo a criação da tabela com o nome de "user"
 public class User {
 
-    public interface CreateUser {
-    } // Inteface para validação do not null, not empty e size
+    public interface CreateUser { // Inteface para validação do not null, not empty e size
+    }
 
-    public interface UpdateUser {
-    } // Inteface para validação do not null, not empty e size
+    public interface UpdateUser { // Inteface para validação do not null, not empty e size
+    }
 
     public static final String TABLE_NAME = "user";
 
     @Id // Definindo o id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    // Anotação estrategio para gerar o id no banco de dados "incremento"
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Anotação estrategio para gerar o id no banco de dados "incremento"
+
     @Column(name = "id", unique = true) // id Unico para caso perca de backup não duplicar valor
 
     // Definindo atributos e anotações do Spring no usuário para gerar as informações no DB
     private Long id;
 
-    @Column(name = "username", length = 100, nullable = false, unique = true)
-    // Definindo o nome da coluna, quantidade maxima de letras, não podendo ser nulo e sendo unico,
+    @Column(name = "username", length = 100, nullable = false, unique = true) // Definindo o nome da coluna, quantidade maxima de letras, não podendo ser nulo e sendo unico,
+
     //Definindo uma segunda validação para o valor não ser uma String vazia nem nulo no username
     @NotNull(groups = CreateUser.class)
     @NotEmpty(groups = CreateUser.class)
-    @Size(groups = CreateUser.class, min = 2, max = 100)
-    // Definindo valores maximos e minimos do username em tempo real
+
+    @Size(groups = CreateUser.class, min = 2, max = 100) // Definindo valores maximos e minimos do username em tempo real
     private String username;
 
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY) // Somente leitura para não retornar a senha no front
-    @Column(name = "password", length = 60, nullable = false)
-    // Definindo o nome da coluna, quantidade maxima de letras, não podendo ser nulo
+    @Column(name = "password", length = 60, nullable = false) // Definindo o nome da coluna, quantidade maxima de letras, não podendo ser nulo
+
     //Definindo uma segunda validação para o valor não ser uma String vazia nem nulo no password
     @NotNull(groups = {CreateUser.class, UpdateUser.class})
     @NotEmpty(groups = {CreateUser.class, UpdateUser.class})
@@ -51,7 +51,6 @@ public class User {
     //private List<Task> taks = new ArrayList<Task> ();
 
     // Criação dos metodos - construtor vazio spring
-
     public User() {
     }
 
@@ -62,7 +61,6 @@ public class User {
     }
 
     // Criação dos Getters E Setters
-
     public Long getId() {
         return id;
     }
