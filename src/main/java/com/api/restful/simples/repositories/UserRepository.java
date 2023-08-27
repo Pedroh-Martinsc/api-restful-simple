21dbb7b5b5002a18407acb578ac8afe8a5d7931b
Pedroh-Martinsc <pedrohenriquem0136@gmail.com>
2023-08-27 16:34:33 -0300

    Feita a criação do repositório do usuário juntamente como todos os modulos necessarios do JpaRepository

--- src/main/java/com/api/restful/simples/repositories/UserRepository.java
+++ src/main/java/com/api/restful/simples/repositories/UserRepository.java
@@ -1,9 +1,10 @@
 package com.api.restful.simples.repositories;
 
+import com.api.restful.simples.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 // Todos métodos já foram incluídos após o extends no JpaRepository..
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository <User, Long> {
 }
